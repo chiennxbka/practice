77b54f1e7896d8186d2ddbc862cde57b2a208c6b
chiennx <chiennx@mintpotvn.com>
2022-08-10 20:00:12 +0700

    update main class

--- src/org/alicoder/lesson22/MainClass.java
+++ src/org/alicoder/lesson22/MainClass.java
@@ -1,9 +1,15 @@
 package org.alicoder.lesson22;
 
+import java.util.*;
+
 public class MainClass {
 
     public static void main(String[] args) {
         Information<String> string = new Information<>("hello");
         System.out.println(string.getValue());
+
+        List<String> strings = new ArrayList<>();
+        Set<Integer> integers = new HashSet<>();
+        Map<String, Object> objectMap = new HashMap<>();
     }
 }
