5a672d294b7d40e381f09c8be8a045260a7169f4
chiennx <chiennx@mintpotvn.com>
2022-08-31 21:16:34 +0700

    insert

--- src/org/alicoder/lesson30/JDBCMain.java
+++ src/org/alicoder/lesson30/JDBCMain.java
@@ -10,9 +10,11 @@ public class JDBCMain {
             connection = DriverManager.getConnection("jdbc:mariadb://database-1.clicssmtjld8.ap-northeast-2.rds.amazonaws.com/classicmodels", "administrator", "6i6Z&9ooKjFK");
             String selectQuery = "select cus.customernumber, cus.customername, cus.phone, cus.country from customers cus";
             String deleteQuery = "DELETE FROM customers WHERE customerNumber=361";
+            String insert = "INSERT INTO classicmodels.offices (officeCode,city,phone,addressLine1,addressLine2,state,country,postalCode,territory)\n" +
+                    "\tVALUES ('8','Viet nam','+84 089 95 478','Ha Noi','Hoai Duc','CA','HN','100000','NA')";
             Statement statement = connection.createStatement();
 //            ResultSet resultSet = statement.executeQuery(deleteQuery); // for select
-            statement.executeQuery(deleteQuery);// for del nad update & insert
+            statement.execute(insert);// for del nad update & insert
             // print with format table
             String leftAlignFormat = "| %-17d | %-29s | %-16s | %-13s |%n";
 
